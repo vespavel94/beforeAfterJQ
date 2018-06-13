4a914a5ab8f555045f81474cafd9920a2e09e2a3
Pavel Veselov <ves.pavel94@gmail.com>
2018-06-13 17:50:00 +0300

    ready to export

---
+++ beforeAfterJQ.js
@@ -0,0 +1,6 @@
+$.prototype.initBeforeAfter = function(){
+    $(this).html('<div class="BA_before"></div>'+
+    '<div class="BA_after"></div>'+
+    '<div class="BA_slider">');
+    $(".BA_slider").initSlider({range: {min:0, max: 100}, flag: "beforeAfter"});
+}
