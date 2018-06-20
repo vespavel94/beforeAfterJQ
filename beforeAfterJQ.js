c8d8c61cdf6dbd1ab1151d2389331af25dd65ca5
Pavel Veselov <ves.pavel94@gmail.com>
2018-06-20 11:38:33 +0300

    js_upd

--- beforeAfterJQ.js
+++ beforeAfterJQ.js
@@ -1,6 +1,8 @@
-$.prototype.initBeforeAfter = function(){
-    $(this).html('<div class="BA_before"></div>'+
-    '<div class="BA_after"></div>'+
-    '<div class="BA_slider">');
-    $(".BA_slider").initSlider({range: {min:0, max: 100}, flag: "beforeAfter"});
-}
+(function($){
+	$.fn.initBeforeAfter = function(){
+    	$(this).html('<div class="BA_before"></div>'+
+    	'<div class="BA_after"></div>'+
+    	'<div class="BA_slider">');
+    	$(".BA_slider").initSlider({range: {min:0, max: 100}, flag: "beforeAfter"});
+	}
+})(jQuery);
